cff2ffc30f24ba9b7dbd77270e494edf9e63199a
Bogdan N <mr.niculec@gmail.com>
2020-09-03 19:47:21 +0300

    Commit #1

---
+++ java2_1598811729.java
@@ -0,0 +1,14 @@
+// Your First Program
+//class
+class javac2 {
+    
+/*method*/ public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int aInt = 3;
+        int bInt = 12;
+        int opInt = aInt+bInt;        
+        System.out.println("a = " + aInt + " | b =" + bInt + " | op =" + opInt);    
+               System.out.println("Git save1 ");    
+
+}
+}
